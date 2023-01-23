8cce4d2b22ee6cb41a0e355b46d68caa0260f085
raacee <raceldamechli@outlook.fr>
2023-01-23 19:00:41 +0100

    removed paths array

--- Company/Driver.cs
+++ Company/Driver.cs
@@ -3,7 +3,7 @@ namespace Company
     public class Driver : Employee
     {
         private bool _isWorking;
-        public Driver(string ssNum, string firstName, string lastName, DateTime birthDate, string phone, string address, string email, DateTime entryTime, string position, int salary, bool isWorking, Sex sex) : base(ssNum, firstName, lastName, birthDate, phone, address, email, entryTime, "Driver", salary, sex, new List<Employee>(0), Array.Empty<string>())
+        public Driver(string ssNum, string firstName, string lastName, DateTime birthDate, string phone, string address, string email, DateTime entryTime, string position, int salary, bool isWorking, Sex sex) : base(ssNum, firstName, lastName, birthDate, phone, address, email, entryTime, "Driver", salary, sex, new List<Employee>(0))
         {
             _isWorking = isWorking;
         }
