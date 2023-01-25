953a0f621c7d8c5a79d79d7867d26d7d71b07bed
raacee <raceldamechli@outlook.fr>
2023-01-25 14:19:03 +0100

    added vehicle field to Driver

--- Company/Driver.cs
+++ Company/Driver.cs
@@ -3,9 +3,11 @@ namespace Company
     public class Driver : Employee
     {
         private bool _isWorking;
-        public Driver(string ssNum, string firstName, string lastName, DateTime birthDate, string phone, string address, string email, DateTime entryTime, string position, int salary, bool isWorking, Sex sex) : base(ssNum, firstName, lastName, birthDate, phone, address, email, entryTime, "Driver", salary, sex, new List<Employee>(0))
+        private Vehicle _vehicle;
+        public Driver(string ssNum, string firstName, string lastName, DateTime birthDate, string phone, string address, string email, DateTime entryTime, string position, int salary, bool isWorking, Sex sex, Vehicle vehicle) : base(ssNum, firstName, lastName, birthDate, phone, address, email, entryTime, "Driver", salary, sex, new List<Employee>(0))
         {
             _isWorking = isWorking;
+            _vehicle = vehicle;
         }
     }
 }
